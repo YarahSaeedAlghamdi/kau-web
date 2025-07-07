33625107d53cb43e7701c81d7906dffe9e2a527c
jood-f <joodfahad21@gmail.com>
2025-07-07 09:14:30 +0300

    create by jood

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@
 import React from 'react'
 import './App.css'
 import NewsSection from './components/NewsSection'
-
+import SaudiMapAnimated from "./SaudiMapAnimated";
 function App() {
   return (
     <div className="App">
       {/* You can add other sections/components here */}
+      <SaudiMapAnimated />
       <NewsSection />
     </div>
   )
+
 }
 
 export default App
+
+
+
+
